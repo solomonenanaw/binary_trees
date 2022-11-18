abbb279f832ab77cf6cb36d38d7656133d773200
solomonenanaw <sekewbeg@gmail.com>
2022-11-18 08:39:36 +0000

    fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
  * @tree: tree to count the nodes from
@@ -47,7 +46,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
-=======
+
 #include "binary_trees.h"
 
 /**
